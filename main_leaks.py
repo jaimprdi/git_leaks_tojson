8f8b3ed1d685dee62d84baa71f7bd5ce87afbf56
jaimprdi <118428381+jaimprdi@users.noreply.github.com>
2022-11-17 09:46:45 +0100

    Add files via upload

---
+++ main_leaks.py
@@ -0,0 +1,68 @@
+from git import Repo 
+import json
+import re 
+import sys
+
+# primero he clonado el repositorio a la carpeta correspondiente 
+
+
+REPO_URL = 'https://github.com/skalenetwork/skale-manager'
+REPO_PATH = 'skale__manager_final'
+
+
+def clonar(REPO_URL):
+    # clonar aqui la url (si se ejecuta ahora no funcionara ya que el archio 'skale__manager_final' ya existe y no esta vacia
+    # si se quiere ejecutar perfecto, cambiar el nomre del repo path y el to_path
+    repo = Repo.clone_from(url=REPO_URL, to_path='skale__manager_final')
+    return 
+
+def salida_controlada():
+    print("\nEnd program")
+    sys.exit()
+
+# ETL- cargar los commits a un json (bloque 1 y 3) 
+def extract(REPO_PATH):
+    repo = Repo(REPO_PATH)
+    #nos creamos la lista de commits 
+    commits = list(repo.iter_commits('develop'))
+    
+    return commits
+
+def transform(commits,palabras):
+    diccionario = {}
+    for i in commits:
+        for j in palabras:
+            if re.search( j, i.message, re.IGNORECASE):
+                #guardamos el par clave:valor en el diccionario
+                data_1 = i.message
+                keys  = i.hexsha
+                #insercion en el diccionario
+                diccionario[keys] = data_1
+
+    return diccionario
+
+def load(diccionario):
+    #abrimos especificando el nombre del archivo, por eso no se lo pasamos a la funcion 
+    archivo=open('commits_to.json', 'w')  
+    #cargamos los commits al json 
+    json.dump(diccionario, archivo, indent=3)
+
+
+def main():
+    
+    palabras = ['credentials', 'password', 'key', 'username']
+    
+    clonar(REPO_URL)
+    
+    commits = extract(REPO_PATH)
+    
+    diccionario = transform(commits,palabras)
+    
+    load(diccionario)
+    
+    salida_controlada()
+    
+
+if __name__  == '__main__' :
+    main()
+    
